b892213d65c238cf969b8448028e2d9625ca4cdc
salvadorjhai <salvadorjhai@outlook.com>
2024-11-07 17:00:04 +0800

    Add project files.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.cboPrinter3 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cboPrinter2 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboPrinter1 = New System.Windows.Forms.ComboBox()
+        Me.txtPort = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.chkAutoStart = New System.Windows.Forms.CheckBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(219, 200)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 28)
+        Me.btnStart.TabIndex = 2
+        Me.btnStart.Text = "&Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(300, 200)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 28)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(368, 184)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.chkAutoStart)
+        Me.TabPage1.Controls.Add(Me.cboPrinter3)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.cboPrinter2)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.cboPrinter1)
+        Me.TabPage1.Controls.Add(Me.txtPort)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(360, 158)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "General"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'cboPrinter3
+        '
+        Me.cboPrinter3.FormattingEnabled = True
+        Me.cboPrinter3.Location = New System.Drawing.Point(100, 96)
+        Me.cboPrinter3.Name = "cboPrinter3"
+        Me.cboPrinter3.Size = New System.Drawing.Size(240, 21)
+        Me.cboPrinter3.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Printer 3:"
+        '
+        'cboPrinter2
+        '
+        Me.cboPrinter2.FormattingEnabled = True
+        Me.cboPrinter2.Location = New System.Drawing.Point(100, 68)
+        Me.cboPrinter2.Name = "cboPrinter2"
+        Me.cboPrinter2.Size = New System.Drawing.Size(240, 21)
+        Me.cboPrinter2.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Printer 2:"
+        '
+        'cboPrinter1
+        '
+        Me.cboPrinter1.FormattingEnabled = True
+        Me.cboPrinter1.Location = New System.Drawing.Point(100, 40)
+        Me.cboPrinter1.Name = "cboPrinter1"
+        Me.cboPrinter1.Size = New System.Drawing.Size(240, 21)
+        Me.cboPrinter1.TabIndex = 6
+        '
+        'txtPort
+        '
+        Me.txtPort.Location = New System.Drawing.Point(100, 12)
+        Me.txtPort.Name = "txtPort"
+        Me.txtPort.Size = New System.Drawing.Size(80, 21)
+        Me.txtPort.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Listening Port:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Printer 1:"
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(12, 200)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 28)
+        Me.btnQuit.TabIndex = 5
+        Me.btnQuit.Text = "&Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'chkAutoStart
+        '
+        Me.chkAutoStart.AutoSize = True
+        Me.chkAutoStart.Location = New System.Drawing.Point(100, 124)
+        Me.chkAutoStart.Name = "chkAutoStart"
+        Me.chkAutoStart.Size = New System.Drawing.Size(76, 17)
+        Me.chkAutoStart.TabIndex = 11
+        Me.chkAutoStart.Text = "Auto Start"
+        Me.chkAutoStart.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(360, 158)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "About"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 241)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnStart)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmMain"
+        Me.Text = "PRINT SERVER"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboPrinter1 As ComboBox
+    Friend WithEvents txtPort As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cboPrinter2 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents cboPrinter3 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents chkAutoStart As CheckBox
+    Friend WithEvents TabPage2 As TabPage
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,63 @@
+﻿Imports System
+Imports System.Net
+Imports System.IO
+Imports System.Drawing.Printing
+Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
+Imports System.Text
+
+Public Class frmMain
+
+    Class config
+        Property listening_port As Integer = -1
+        Property printer1 As String = ""
+        Property printer2 As String = ""
+        Property printer3 As String = ""
+
+        Private Shared fl As String = ".\appconfig.js"
+        Shared Function load() As config
+            If File.Exists(fl) Then
+                Return JsonConvert.DeserializeObject(Of config)(File.ReadAllText(fl, New UTF8Encoding(False)))
+            End If
+            Return New config
+        End Function
+
+        Function Save() As config
+            Dim js = JsonConvert.SerializeObject(Me, Formatting.Indented)
+            File.WriteAllText(fl, js, New UTF8Encoding(False))
+            Return Me
+        End Function
+    End Class
+
+    Private APP_CONFIG As config = Nothing
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        APP_CONFIG = config.load()
+
+        Me.CenterToScreen()
+
+        loadPrinters()
+
+    End Sub
+
+    Sub loadPrinters()
+        cboPrinter1.Items.Clear()
+        cboPrinter2.Items.Clear()
+        cboPrinter3.Items.Clear()
+
+        Dim l1 = PrinterSettings.InstalledPrinters
+        For i = 0 To l1.Count - 1
+            cboPrinter1.Items.Add(l1(i))
+            cboPrinter2.Items.Add(l1(i))
+            cboPrinter3.Items.Add(l1(i))
+        Next
+
+        ' set values
+        cboPrinter1.Text = APP_CONFIG.printer1
+        cboPrinter2.Text = APP_CONFIG.printer2
+        cboPrinter3.Text = APP_CONFIG.printer3
+
+    End Sub
+
+
+End Class
